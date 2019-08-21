e273115ff7fdfa146ba862dfdd09d9259493bd3a
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-21 09:35:24 +0000

    Added with def

--- jobs/Jenkins_Maven_Sonar_Artifactory_Email_Build.groovy
+++ jobs/Jenkins_Maven_Sonar_Artifactory_Email_Build.groovy
@@ -1,8 +1,12 @@
+def jobDslRepo		=	"https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
+def jobDslBranch	=	"master"
+def jobDslRepoCred	=	"GitCred"
+
 job("Jenkins_Maven_Sonar_Artifactory_Email_Build_Job") {
 	parameters {
-		choiceParam("job_dsl_repo"	, ["https://github.com/vigneshpalanivelr/terraform_practice_codes.git"]	, "Job DSL Repo")
-		choiceParam("job_dsl_branch"	, ["master"]								, "Job DSL Branch")
-		choiceParam("job_dsl_repo_cred"	, ["GitCred"]								, "Job DSL Cred")
+		choiceParam("job_dsl_repo"	, [jobDslRepo]		, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, [jobDslBranch]	, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, [jobDslRepoCred]	, "Job DSL Cred")
 	}
 	scm {
         	git {
@@ -10,7 +14,7 @@ job("Jenkins_Maven_Sonar_Artifactory_Email_Build_Job") {
 			remote {
 			name("origin")
                 	url("\$job_dsl_repo")
-                	credentials("GitCred")
+                	credentials(jobDslRepoCred)
             		}
         	}
     	}
