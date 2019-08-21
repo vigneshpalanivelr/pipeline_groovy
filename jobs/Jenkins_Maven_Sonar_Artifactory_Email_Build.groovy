1f7aad79ffaaead1aae2919efabf8b24684c71d5
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-21 14:31:15 +0530

    Update Jenkins_Maven_Sonar_Artifactory_Email_Build.groovy

--- jobs/Jenkins_Maven_Sonar_Artifactory_Email_Build.groovy
+++ jobs/Jenkins_Maven_Sonar_Artifactory_Email_Build.groovy
@@ -1,4 +1,4 @@
-job("Jenkins_Maven_Sonar_Artifactory_Email_Build") {
+job("Jenkins_Maven_Sonar_Artifactory_Email_Build_Job") {
 	parameters {
 		choiceParam("job_dsl_repo"	, ["https://github.com/vigneshpalanivelr/terraform_practice_codes.git"]	, "Job DSL Repo")
 		choiceParam("job_dsl_branch"	, ["master"]								, "Job DSL Branch")
