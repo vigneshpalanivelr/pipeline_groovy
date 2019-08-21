b433567e68709c474db395e749f9fc322cc3ccd5
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-21 23:20:29 +0530

    Update maven_project.groovy

--- pipeline/maven_project.groovy
+++ pipeline/maven_project.groovy
@@ -1,3 +1,5 @@
+def STASH_USERNAME = "STASH_USERNAME"
+def STASH_PASSWORD = "STASH_PASSWORD"
 node ('master') {
 	writeFile(file:	"git-askpass-${BUILD_TAG}", text:"#!/bin/bash\ncase \"\$1\" in \nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
 	sh "chmod a+x "git-askpass-${BUILD_TAG}
