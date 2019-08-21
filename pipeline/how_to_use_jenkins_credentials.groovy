e78d2c75d06aeae42c34724e4344d49d24483566
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-21 18:43:46 +0000

    Added with def

--- pipeline/how_to_use_jenkins_credentials.groovy
+++ pipeline/how_to_use_jenkins_credentials.groovy
@@ -1,16 +1,20 @@
 node ('master') {
-	writeFile(file:	"git-askpass-${BUILD_TAG}", text:"#!/bin/bash\ncase \"\$1\" in \nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
-	sh "chmod a+x git-askpass-${BUILD_TAG}"
-	withEnv(["GIT_ASKPASS=${WORKSPACE}/git-askpass-${BUILD_TAG}"]){
-		withCredentials([usernamePassword(credentialsId: 'GitCred',passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
-		echo STASH_PASSWORD
-		sh 'echo $STASH_USERNAME'
+	stage('with git-askpass file and case statement') {
+		writeFile(file:	"git-askpass-${BUILD_TAG}", text:"#!/bin/bash\ncase \"\$1\" in \nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
+		sh "chmod a+x git-askpass-${BUILD_TAG}"
+		withEnv(["GIT_ASKPASS=${WORKSPACE}/git-askpass-${BUILD_TAG}"]){
+			withCredentials([usernamePassword(credentialsId: 'GitCred',passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
+			echo STASH_PASSWORD
+			sh 'echo $STASH_USERNAME'
+			}
 		}
 	}
 }
 node ('master') {
-	withCredentials([usernamePassword(credentialsId: job_dsl_repo_cred,passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
-	echo STASH_PASSWORD
-	sh 'echo $STASH_USERNAME'
+	stage('just using the jenkins credentials') {
+		withCredentials([usernamePassword(credentialsId: job_dsl_repo_cred,passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
+		echo STASH_PASSWORD
+		sh 'echo $STASH_USERNAME'
+		}
 	}
 }
