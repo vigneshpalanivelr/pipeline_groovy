13320d6a416be8938d3525054900a95d8a739e7d
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-21 09:09:21 +0000

    Added Admin seed job

--- admin-seed-job.groovy
+++ admin-seed-job.groovy
@@ -1,21 +1,21 @@
 job("admin-seed-job") {
 	description('Job to create Admin Seed Jobs')
 	parameters {
-		choiceParam("job_dsl_repo"		, ["https://github.com/vigneshpalanivelr/pipeline_groovy.git"], "Job DSL Repo")
-		choiceParam("job_dsl_branch"	, ["master"], "Job DSL Branch")
-		choiceParam("job_dsl_repo_cred"	, ["GitCred"], "Job DSL Cred")
-		choiceParam("job_dsl_path"		, ["jobs/**/*.groovy"], "Location of Job DSL Groovy Script")
+		choiceParam("job_dsl_repo"	, ["https://github.com/vigneshpalanivelr/pipeline_groovy.git"]	, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, ["master"]							, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, ["GitCred"]							, "Job DSL Cred")
+		choiceParam("job_dsl_path"	, ["jobs/**/*.groovy"]						, "Location of Job DSL Groovy Script")
 	}
 	scm {
         	git {
 			branch("\$job_dsl_branch")
 			remote {
 			name("origin")
-            url("\$job_dsl_repo")
-            credentials("GitCred")
+			url("\$job_dsl_repo")
+			credentials("GitCred")
 			}
 		}
-    }
+	}
 	steps {
 		dsl {
 			external("\$job_dsl_path")
