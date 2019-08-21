38a909a34d36f52e9b86fc882aa23dfde5bb277d
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-21 18:16:34 +0000

    Added with def

--- pipeline/maven_project.groovy
+++ pipeline/maven_project.groovy
@@ -1,6 +1,9 @@
-def STASH_USERNAME = "STASH_USERNAME"
-def STASH_PASSWORD = "STASH_PASSWORD"
 node ('master') {
 	writeFile(file:	"git-askpass-${BUILD_TAG}", text:"#!/bin/bash\ncase \"\$1\" in \nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
 	sh "chmod a+x git-askpass-${BUILD_TAG}"
+	withEnv(["GIT_ASKPASS"=${WORKSPACE}/git-askpass-${BUILD_TAG}]){
+		withCredentials([usernamePassword(credentialsId: gitCreds,passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')])
+		echo $STASH_PASSWORD
+		echo $STASH_USERNAME
+	}
 }
