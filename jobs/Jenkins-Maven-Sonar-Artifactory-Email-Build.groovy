c2040f66e84b6beb63b39d4c3a4fdcd67531ea38
root <root@ip-172-31-46-153.ap-south-1.compute.internal>
2019-08-21 08:10:28 +0000

    Added 1st Groovy

---
+++ jobs/Jenkins-Maven-Sonar-Artifactory-Email-Build.groovy
@@ -0,0 +1,17 @@
+job("Jenkins-Maven-Sonar-Artifactory-Email-Build") {
+	parameters {
+		choiceParam("job_dsl_repo"	, ["https://github.com/vigneshpalanivelr/terraform_practice_codes.git"]	, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, ["master"]								, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, ["GitCred"]								, "Job DSL Cred")
+	}
+	scm {
+        	git {
+			branch("\$job_dsl_branch")
+			remote {
+			name("origin")
+                	url("\$job_dsl_repo")
+                	credentials("GitCred")
+            		}
+        	}
+    	}
+}
