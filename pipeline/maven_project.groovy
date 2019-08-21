fa7514600b3727aed96507e03473d05216f2db28
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-21 18:34:15 +0000

    Added with def

--- pipeline/maven_project.groovy
+++ pipeline/maven_project.groovy
@@ -9,10 +9,8 @@ node ('master') {
 	}
 }
 node ('master') {
-        withEnv(["GIT_ASKPASS=${WORKSPACE}/git-askpass-${BUILD_TAG}"]){
-                withCredentials([usernamePassword(credentialsId: 'GitCred',passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
-                echo STASH_PASSWORD
-                sh 'echo $STASH_USERNAME'
-                }
-        }
+	withCredentials([usernamePassword(credentialsId: 'GitCred',passwordVariable: 'STASH_PASSWORD',usernameVariable: 'STASH_USERNAME')]) {
+	echo STASH_PASSWORD
+	sh 'echo $STASH_USERNAME'
+	}
 }
