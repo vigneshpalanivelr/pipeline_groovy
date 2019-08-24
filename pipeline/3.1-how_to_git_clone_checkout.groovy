f3db6ed1165623fa747781289f7cadd2f013a96d
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 16:33:48 +0530

    Update 3.1-how_to_git_clone_checkout.groovy

--- pipeline/3.1-how_to_git_clone_checkout.groovy
+++ pipeline/3.1-how_to_git_clone_checkout.groovy
@@ -1,5 +1,16 @@
 node('master') {
 	stage('Clone & Checkout') {
-		checkout([$class: 'GitSCM', branches: [[name: gitBranch ]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: gitCreds, url: gitRepo]]])
+		checkout([checkout([
+			$class: 'GitSCM', 
+			branches: [[name: gitBranch ]], 
+			doGenerateSubmoduleConfigurations: false, 
+			extensions: [[$class: 'CleanCheckout'], [
+				$class: 'SubmoduleOption', 
+				disableSubmodules: false, 
+				parentCredentials: true, 
+				recursiveSubmodules: true, 
+				reference: '', trackingSubmodules: false]], 
+			submoduleCfg: [], 
+			userRemoteConfigs: [[credentialsId: gitCreds, url: gitRepo], []]])
 	}
 }
