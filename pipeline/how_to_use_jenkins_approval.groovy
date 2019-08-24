fa514c8c339126842e5a60a57345e43588558a11
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 12:57:20 +0530

    Update how_to_use_jenkins_approval.groovy

--- pipeline/how_to_use_jenkins_approval.groovy
+++ pipeline/how_to_use_jenkins_approval.groovy
@@ -8,7 +8,6 @@ try {
         id: 'Proceed1', message: 'Was this successful?', parameters: [
         [$class: 'BooleanParameterDefinition', defaultValue: true, description: '', name: 'Please confirm you agree with this']
         ])
-        echo "${userInput}"
     }
 }
 catch(err) { 
@@ -16,29 +15,37 @@ catch(err) {
     echo "${err}"
     cause = err.causes.get(0)
     echo "${cause}"
-    if (cause instanceof org.jenkinsci.plugins.workflow.steps.TimeoutStepExecution.ExceededTimeout) {
-      didTimeout = true
-      println('ExceededTimeout!')
-    } 
-    else if (cause instanceof org.jenkinsci.plugins.workflow.support.steps.input.Rejection){
-      userInput = false
-      def user = err.getCauses()[0].getUser()
-      echo "${user}"
-      println('Rejection!')
-    }
+    if (cause instanceof org.jenkinsci.plugins.workflow.support.steps.input.Rejection){
+        def user = err.getCauses()[0].getUser()
+        echo "${user}"
+        if('SYSTEM' == user.toString()) {
+            // SYSTEM   means timeout 
+            // USERNAME means aborted (any)
+            didTimeout = true
+        }
+        else {
+            userInput = false
+            echo "Aborted by: [${user}]"
+        }
     else {
-      println('error inside FlowInterruptedException: ' + err)
+        println('error inside FlowInterruptedException: ' + err)
     }
 }
 
 node {
     if (didTimeout) {
-        echo "no input was received before timeout"
-        currentBuild.result = 'ABORTED'
-    } else if (userInput == true) {
-        echo "this was successful"
-    } else {
-        echo "this was not successful"
+        echo "No input was received : TIMEOUT"
         currentBuild.result = 'ABORTED'
     } 
+    else if (userInput == true) {
+        echo "Successful"
+    } 
+    else if (userInput == false) {
+        echo "Input received as ABORT"
+        currentBuild.result = 'ABORTED'
+    }
+    else {
+        echo "Build Failed"
+        currentBuild.result = 'FAILURE'
+    }
 }
