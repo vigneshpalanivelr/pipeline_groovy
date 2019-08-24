c6508345c61a6aa183bcd905e4ef193436de4bd3
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 15:23:52 +0530

    Update all_small_jobs_groovy.groovy

--- jobs/all_small_jobs_groovy.groovy
+++ jobs/all_small_jobs_groovy.groovy
@@ -1,14 +1,14 @@
-def jobDslRepo		=	"https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
-def jobDslBranch	=	"master"
-def jobDslRepoCred	=	"GitCred"
+def gitRepo		=	"https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
+def gitBranch		=	"master"
+def gitCreds		=	"GitCred"
 
 pipelineJob("1.1-how_to_use_jenkins_credentials_build_job") {
 	description ('Explains how to use Credentials for Build Jobs')
 	logRotator(-1,-1)
 	parameters {
-		choiceParam("job_dsl_repo"	, [jobDslRepo]		, "Job DSL Repo")
-		choiceParam("job_dsl_branch"	, [jobDslBranch]	, "Job DSL Branch")
-		choiceParam("job_dsl_repo_cred"	, [jobDslRepoCred]	, "Job DSL Cred")
+		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
 	}
 	definition {
 		cps {
@@ -22,13 +22,29 @@ pipelineJob("2.1-how_to_use_jenkins_approval_build_job") {
 	description('Explains how to use Jenins Approval for Build Jobs')
 	logRotator(-1,-1)
 	parameters{
-                choiceParam("job_dsl_repo"      , [jobDslRepo]          , "Job DSL Repo")
-                choiceParam("job_dsl_branch"    , [jobDslBranch]        , "Job DSL Branch")
-                choiceParam("job_dsl_repo_cred" , [jobDslRepoCred]      , "Job DSL Cred")
+		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
         }
         definition {
                 cps {
                         script(readFileFromWorkspace('pipeline/2.1-how_to_use_jenkins_approval.groovy'))
+                        sandbox()
+                }
+	}
+}
+
+pipelineJob("3.1-how_to_git_clone_checkout_build_job") {
+	description('Explains how to use Jenins Approval for Build Jobs')
+	logRotator(-1,-1)
+	parameters{
+		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
+		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/3.1-how_to_git_clone_checkout.groovy'))
                         sandbox()
                 }
 	}
