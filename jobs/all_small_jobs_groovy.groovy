1f0625cfbfc3dee84b4f8315974134450f221abc
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 15:39:44 +0530

    Update all_small_jobs_groovy.groovy

--- jobs/all_small_jobs_groovy.groovy
+++ jobs/all_small_jobs_groovy.groovy
@@ -1,12 +1,12 @@
-def gitRepo		=	"https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
-def gitBranch		=	"*/master"
+def gitRepo			=	"https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
+def gitBranch		=	"master"
 def gitCreds		=	"GitCred"
 
 pipelineJob("1.1-how_to_use_jenkins_credentials_build_job") {
 	description ('Explains how to use Credentials for Build Jobs')
 	logRotator(-1,-1)
 	parameters {
-		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_repo"		, [gitRepo]		, "Job DSL Repo")
 		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
 		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
 	}
@@ -22,15 +22,15 @@ pipelineJob("2.1-how_to_use_jenkins_approval_build_job") {
 	description('Explains how to use Jenins Approval for Build Jobs')
 	logRotator(-1,-1)
 	parameters{
-		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_repo"		, [gitRepo]		, "Job DSL Repo")
 		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
 		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
-        }
-        definition {
-                cps {
-                        script(readFileFromWorkspace('pipeline/2.1-how_to_use_jenkins_approval.groovy'))
-                        sandbox()
-                }
+	}
+	definition {
+		cps {
+			script(readFileFromWorkspace('pipeline/2.1-how_to_use_jenkins_approval.groovy'))
+			sandbox()
+		}
 	}
 }
 
@@ -38,14 +38,14 @@ pipelineJob("3.1-how_to_git_clone_checkout_build_job") {
 	description('Explains how to use Jenins Approval for Build Jobs')
 	logRotator(-1,-1)
 	parameters{
-		choiceParam("job_dsl_repo"	, [gitRepo]	, "Job DSL Repo")
+		choiceParam("job_dsl_repo"		, [gitRepo]		, "Job DSL Repo")
 		choiceParam("job_dsl_branch"	, [gitBranch]	, "Job DSL Branch")
 		choiceParam("job_dsl_repo_cred"	, [gitCreds]	, "Job DSL Cred")
-        }
-        definition {
-                cps {
-                        script(readFileFromWorkspace('pipeline/3.1-how_to_git_clone_checkout.groovy'))
-                        sandbox()
-                }
+	}
+	definition {
+		cps {
+			script(readFileFromWorkspace('pipeline/3.1-how_to_git_clone_checkout.groovy'))
+			sandbox()
+		}
 	}
 }
