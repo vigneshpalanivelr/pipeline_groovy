68b5170c299c81659fbfcc5d5bf2f46e59334882
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 14:33:51 +0530

    Update 2.2-how_to_use_jenkins_approval.groovy

--- pipeline/2.2-how_to_use_jenkins_approval.groovy
+++ pipeline/2.2-how_to_use_jenkins_approval.groovy
@@ -5,11 +5,11 @@ node('master'){
   stage('echo') {
     echo "Approved"
   }
-}
 
 def approval() {
   timeout(5){
     // Every input step has an unique ID. It is used in the generated URL to proceed or abort.
     input id: "Deploy Gate", message: "Deploy ?", ok: 'Deploy'
   }
+}
 }
