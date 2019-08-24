7e1e624c5833f9a57e561fea177398fcda3fe168
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 14:18:54 +0530

    Update 2.2-how_to_use_jenkins_approval.groovy

--- pipeline/2.2-how_to_use_jenkins_approval.groovy
+++ pipeline/2.2-how_to_use_jenkins_approval.groovy
@@ -10,6 +10,6 @@ node('master'){
 def approval() {
   timeout(time: 15, unit: 'SECONDS'){
     // Every input step has an unique ID. It is used in the generated URL to proceed or abort.
-    input{'You want to Continue ?'}
+    input{message 'You want to Continue ?'}
   }
 }
