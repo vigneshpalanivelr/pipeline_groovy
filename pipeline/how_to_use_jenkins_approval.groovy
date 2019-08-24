98809f49c89373cade4754f388d109f986b50cda
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 11:42:54 +0530

    Update how_to_use_jenkins_approval.groovy

--- pipeline/how_to_use_jenkins_approval.groovy
+++ pipeline/how_to_use_jenkins_approval.groovy
@@ -35,39 +35,3 @@ node {
         currentBuild.result = 'FAILURE'
     } 
 }
-
-userAborted		= false
-startMillis 	= System.currentTimeMillis()
-timeoutMillis	= 10000
-
-try { 
-	timeout(time: timeoutMillis, unit: 'MILLISECONDS') {
-		input 'Do you approve?' 
-	} 
-}
-catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
-	cause = e.causes.get(0)
-	echo "${cause}"
-	echo "Aborted by " + cause.getUser().toString()
-	if (cause.getUser().toString() != 'SYSTEM') {
-		startMillis = System.currentTimeMillis()
-	}
-	else {
-		endMillis = System.currentTimeMillis()
-		if (endMillis - startMillis >= timeoutMillis) {
-			echo "Approval timed out. Continuing with deployment."
-		}
-		else {
-			userAborted = true
-			echo "SYSTEM aborted, but looks like timeout period didn't complete. Aborting."
-		}
-	}
-}
-
-if (userAborted) {
-	currentBuild.result = 'ABORTED'
-} 
-else {
-	currentBuild.result = 'SUCCESS'
-	echo "Firing the missiles!"
-}
