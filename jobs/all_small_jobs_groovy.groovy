9f5698fac8557d00891e889bf7037e24a513cd3a
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-24 13:47:51 +0530

    Update all_small_jobs_groovy.groovy

--- jobs/all_small_jobs_groovy.groovy
+++ jobs/all_small_jobs_groovy.groovy
@@ -33,3 +33,19 @@ pipelineJob("2.1-how_to_use_jenkins_approval_build_job") {
                 }
 	}
 }
+
+pipelineJob("2.2-how_to_use_jenkins_approval_build_job") {
+	description('Explains how to use Jenins Approval for Build Jobs')
+	logRotator(-1,-1)
+	parameters{
+                choiceParam("job_dsl_repo"      , [jobDslRepo]          , "Job DSL Repo")
+                choiceParam("job_dsl_branch"    , [jobDslBranch]        , "Job DSL Branch")
+                choiceParam("job_dsl_repo_cred" , [jobDslRepoCred]      , "Job DSL Cred")
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/2.2-how_to_use_jenkins_approval.groovy'))
+                        sandbox()
+                }
+	}
+}
