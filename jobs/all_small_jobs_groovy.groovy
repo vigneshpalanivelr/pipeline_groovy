2760d4db18e1fc4cee5670d9859af3a9af04cf00
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-24 05:23:49 +0000

    Added few enhancements

--- jobs/all_small_jobs_groovy.groovy
+++ jobs/all_small_jobs_groovy.groovy
@@ -3,7 +3,7 @@ def jobDslBranch	=	"master"
 def jobDslRepoCred	=	"GitCred"
 
 pipelineJob("how_to_use_jenkins_credentials_build_job") {
-	description ('Explains how tio use Credentials for Build Jobs')
+	description ('Explains how to use Credentials for Build Jobs')
 	logRotator(-1,-1)
 	parameters {
 		choiceParam("job_dsl_repo"	, [jobDslRepo]		, "Job DSL Repo")
@@ -15,5 +15,21 @@ pipelineJob("how_to_use_jenkins_credentials_build_job") {
 			script(readFileFromWorkspace('pipeline/how_to_use_jenkins_credentials.groovy'))
 			sandbox()
 		}
+	}
+}
+
+pipelineJob("how_to_use_jenkins_approval_build_job") {
+	description('Explains how to use Jenins Approval for Build Jobs')
+	logRotator(-1,-1)
+	parameters{
+                choiceParam("job_dsl_repo"      , [jobDslRepo]          , "Job DSL Repo")
+                choiceParam("job_dsl_branch"    , [jobDslBranch]        , "Job DSL Branch")
+                choiceParam("job_dsl_repo_cred" , [jobDslRepoCred]      , "Job DSL Cred")
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/how_to_use_jenkins_approval.groovy'))
+                        sandbox()
+                }
 	}
 }
---
+++ pipeline/how_to_use_jenkins_approval.groovy
@@ -0,0 +1,33 @@
+def userInput	= true
+def didTimeout	= false
+
+try {
+    // change to a convenient timeout for you
+    timeout(time: 15, unit: 'SECONDS') {
+        userInput = input(
+        id: 'Proceed1', message: 'Was this successful?', parameters: [
+        [$class: 'BooleanParameterDefinition', defaultValue: true, description: '', name: 'Please confirm you agree with this']
+        ])
+    }
+} catch(err) { 
+    // timeout reached or input false
+    def user = err.getCauses()[0].getUser()
+    if('SYSTEM' == user.toString()) {
+        // SYSTEM means timeout.
+        didTimeout = true
+    } else {
+        userInput = false
+        echo "Aborted by: [${user}]"
+    }
+}
+
+node {
+    if (didTimeout) {
+        echo "no input was received before timeout"
+    } else if (userInput == true) {
+        echo "this was successful"
+    } else {
+        echo "this was not successful"
+        currentBuild.result = 'FAILURE'
+    } 
+}
