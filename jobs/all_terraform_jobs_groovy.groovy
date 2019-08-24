2d2517aeeb30bcf2d32bc61dd0eadbffddfe115c
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-24 16:53:27 +0000

    Added RDS Groovy Job and pipeline

---
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -0,0 +1,37 @@
+def terraformRepo       	= "https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
+def terraformBranch     	= "master"
+def gitCreds            	= "GitCred"
+terraformTFstateBucket		= "terraform-tfstate-mumbai"
+terraformTFstateBucketPrefix	= "rds_module"
+
+pipelineJob('rds_build_job') {
+        description('Explains how to use Jenins Approval for Build Jobs')
+        logRotator(-1,-1)
+        parameters{
+                choiceParam('gitRepo'           	, [terraformRepo]       	, 'Job DSL Repo')
+                choiceParam('gitBranch'         	, [terraformBranch]     	, 'Job DSL Branch')
+                choiceParam('gitCreds'          	, [gitCreds]            	, 'Job DSL Cred')
+		choiceParam('tfstateBucket'		, [terraformTFstateBucket]	, 'TF State Bucket')
+		choiceParam('tfstateBucketPrefix'	, [terraformTFstateBucketPrefix], 'TF State Bucket Prefix')
+                choiceParam('db_engine')        	, ['postgres','oracle','mssql','mariadb','mysql','aurora']      , 'db type')
+                stringParam('db_family'         	, 'db_family'           	, 'db engine_family')
+		stringParam('db_engine_version' 	, 'db_version'          	, 'db engine_version')
+                choiceParam('db_instance_class'		, ['db.t2.small']		, 'db instance type')
+		stringParam('db_identifier')    	, 'instance_name'       	, 'db instance name'
+		stringParam('db_name'			, 'db_name'          		, 'db name')
+		stringParam('db_username'		, 'db_username'        		, 'db username')
+		stringParam('db_password'		, 'db_password'        		, 'db password')
+		stringParam('db_allocated_storage'	, 'db_storage'			, 'db_allocated_storage')
+		choiceParam('db_multi_az'		, ['true','false']		, 'db_multi_az')
+		choiceParam('createInstance'		, ['true','false']		, 'create_db_instance')
+		stringParam('db_R53_name'		, 'R53_Name'			, 'db_route53_name')
+		choiceParam('createInstanceDNS'		, ['true','false']		, 'creating_db_instance_dns')
+		choiceParam('terraformApplyPlan'	, ['true','false']		, 'terraform_apply_plan')
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/rds_build.groovy'))
+                        sandbox()
+                }
+        }
+}
---
+++ pipeline/rds_build.groovy
@@ -0,0 +1,70 @@
+/*
+*	gitRepo
+*	gitBranch
+*	gitCreds
+*	tfstateBucket
+*	tfstateBucketPrefix
+
+*	db_engine
+*	db_family
+*	db_engine_version
+*	db_instance_class
+*	db_identifier
+*	db_name
+*	db_username
+*	db_password
+*	db_allocated_storage
+*	db_multi_az
+*	db_R53_name
+
+*	createInstance
+*	createInstanceDNS
+*	terraformApplyPlan
+*/
+
+node('master') {
+	def terraformDirectory	= "modules/all_modules/rds_module"
+	
+	withFile(file: "git-askpass-${BUILD_TAG}",text:"#!/bin/bash\ncase \"\$1\" in\nUsername*) echo \"${STASH_USERNAME}\" ;;\nPassword*) \"${STASH_PASSWORD}\" ;;\nesac")
+	sh "chmod a+x git-askpass-${BUILD_TAG}"
+	
+	stage('Approve before Start'){
+		approval()
+	}
+	stage('Checkout') {
+		checkout()
+	}
+}
+
+def approval() {
+	timeout(time:15, unit:SECONDS) {
+		input(
+			id: 'Approval',
+			message: 'Shall i continue ?',
+			parameters:	[[
+				$class:	'BooleanParameterDefinition', 
+				defaultValue: true, 
+				description: 'default to tick', 
+				name: 'Please confirm to proceed'
+			]]
+		)
+	}
+}
+
+def checkout() {
+	checkout([
+		$class: 'GitSCM', 
+		branches: [[name: gitBranch ]], 
+		doGenerateSubmoduleConfigurations: false, 
+		clearWorkspace: true,
+		extensions: [
+			[$class: 'CleanCheckout'], [
+			$class: 'SubmoduleOption', 
+			disableSubmodules: false, 
+			parentCredentials: true, 
+			recursiveSubmodules: true, 
+			reference: '', trackingSubmodules: false]], 
+		submoduleCfg: [], 
+		userRemoteConfigs: [[credentialsId: gitCreds, url: gitRepo]]
+	])
+}
