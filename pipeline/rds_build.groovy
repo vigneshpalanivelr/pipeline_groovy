d82f38f104e2914fd840876f8e6748975a137fd2
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-25 06:28:32 +0000

    Added RDS variable file

--- pipeline/rds_build.groovy
+++ pipeline/rds_build.groovy
@@ -41,7 +41,19 @@ node('master') {
 				stage('RDS Instance Plan') {
 					global_tfvars	= "../../../global_vars.tfvars"
 					rds_tfvars	= "../../../${db_engine}.tfvars"
-					terraform_plan(global_tfvars,rds_tfvars)
+					withEnv(["TF_VAR_db_password=${db_password}"]){
+						env.TF_VAR_db_engine		= "${db_engine}"
+                                                env.TF_VAR_db_family		= "${db_family}"
+                                                env.TF_VAR_db_engine_version	= "${db_engine_version}"
+                                                env.TF_VAR_db_instance_class	= "${db_instance_class}"
+                                                env.TF_VAR_db_identifier	= "${db_identifier}"
+						env.TF_VAR_db_name		= "${db_name}"
+						env.TF_VAR_db_username		= "${db_username}"
+						env.TF_VAR_db_allocated_storage	= "${db_allocated_storage}"
+						env.TF_VAR_db_multi_az		= "${db_multi_az}"
+						env.TF_VAR_db_R53_name		= "${db_R53_name}"
+						terraform_plan(global_tfvars,rds_tfvars)
+					}
 				}
 			}
 		}
