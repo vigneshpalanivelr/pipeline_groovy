06ba1c7c8e84623a4e3884347c94e7f17e0098e2
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-25 19:23:19 +0000

    Added RDS Gen

--- pipeline/rds_build_gen.groovy
+++ pipeline/rds_build_gen.groovy
@@ -23,7 +23,7 @@
 */
 
 node('master') {
-	def terraformDirectoryRDS	= "modules/all_modules/rds_module"
+	def terraformDirectoryRDS	= "modules/all_modules/rds_module_oracle"
 	global_tfvars   		= "../../../global_vars.tfvars"
 	rds_tfvars      		= "../../../${db_engine}.tfvars"
 	env.TF_LOG			= "${tfLogLevel}"
