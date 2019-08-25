73d9b60b31982105060c466f8afc12f17385de97
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-08-25 18:50:33 +0000

    Added RDS Gen

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -36,3 +36,36 @@ pipelineJob('rds_build_job') {
                 }
         }
 }
+
+pipelineJob('rds_build_gen_job') {
+        description('Explains how to use Jenins Approval for Build Jobs')
+        logRotator(-1,-1)
+        parameters{
+                choiceParam('gitRepo'                   , [terraformRepo]       , '')
+                choiceParam('gitBranch'                 , [terraformBranch]     , '')
+                choiceParam('gitCreds'                  , [gitCreds]            , '')
+                choiceParam('tfstateBucket'             , [terraformTFstateBucket]      , 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [terraformTFstateBucketPrefix], 'TF State Bucket Prefix'      )
+                choiceParam('tfLogLevel'                , ['ERROR','WARN','INFO','DEBUG','TRACE'],                      ,'')
+                choiceParam('db_engine'                 , ['postgres','oracle','mssql','mariadb','mysql','aurora']      ,'')
+                stringParam('db_family'                 , 'db_family'           , '')
+                stringParam('db_engine_version'         , 'db_version'          , '')
+                choiceParam('db_instance_class'         , ['db.t2.small']       , '')
+                stringParam('db_identifier'             , 'instance_name'       , '')
+                stringParam('db_name'                   , 'db_name'             , '')
+                choiceParam('db_username'               , ['Administrator']     , '')
+                nonStoredPasswordParam('db_password'    , 'Do you think that you can see !!')
+                stringParam('db_allocated_storage'      , '5'                   , 'in GBs')
+                choiceParam('db_multi_az'               , ['false','true']      , '')
+                choiceParam('createInstance'            , ['true','false']      , '')
+                stringParam('db_R53_name'               , 'R53_Name'            , '')
+                choiceParam('createInstanceDNS'         , ['true','false']      , '')
+                choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']     , '')
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/rds_build_gen.groovy'))
+                        sandbox()
+                }
+        }
+}
---
+++ pipeline/rds_build_gen.groovy
@@ -0,0 +1,141 @@
+/*
+*	gitRepo
+*	gitBranch
+*	gitCreds
+*	tfstateBucket
+*	tfstateBucketPrefix
+
+*	db_engine
+*	db_family
+*	db_engine_version
+*	db_instance_class
+*	db_identifier
+*	db_name
+*	db_username
+*	db_password
+*	db_allocated_storage
+*	db_multi_az
+*	db_R53_name
+
+*	createInstance
+*	createInstanceDNS
+*	terraformApplyPlan
+*/
+
+node('master') {
+	def terraformDirectoryRDS	= "modules/all_modules/rds_module"
+	global_tfvars   		= "../../../global_vars.tfvars"
+	rds_tfvars      		= "../../../${db_engine}.tfvars"
+	env.TF_LOG			= "${tfLogLevel}"
+	env.TF_LOG_PATH			= "terraform.txt"
+	
+	writeFile(file: "askp-${BUILD_TAG}",text:"#!/bin/bash\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) \"\${STASH_PASSWORD}\" ;;\nesac")
+	sh "chmod a+x askp-${BUILD_TAG}"
+	
+	stage('Approve before Start'){
+		approval()
+	}
+	stage('Checkout') {
+		checkout()
+		if (createInstance == 'true'){
+			dir(terraformDirectoryRDS){
+				stage('Remote State Init') {
+					terraform_init()
+				}
+				if (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply') {
+					stage('Terraform Plan'){
+						withEnv(["TF_VAR_db_password=${db_password}"]){
+							set_env_variables()
+                		               		terraform_plan(global_tfvars,rds_tfvars)
+						}
+					}
+				}
+				if (terraformApplyPlan == 'apply') {
+					stage('Plan Approve & Apply'){
+						approval()
+						terraform_apply()
+					}
+				}
+				if (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy') {
+					stage('Plan Destroy'){
+                                                withEnv(["TF_VAR_db_password=${db_password}"]){
+							set_env_variables()
+	        	                        	terraform_plan_destroy(global_tfvars,rds_tfvars)
+						}
+					}
+				}
+				if (terraformApplyPlan == 'destroy') {
+					stage('Approve & Destroy'){
+                                                approval()
+                                                terraform_destroy()
+                                	}
+				}
+			}
+		}
+	}
+}
+
+def approval() {
+	timeout(time: 15, unit: 'SECONDS') {
+		input(
+			id: 'Approval', message: 'Shall I Continue ?', parameters: [[
+				$class:	'BooleanParameterDefinition', defaultValue: true, description: 'default to tick', name: 'Please confirm to proceed'
+			]]
+		)
+	}
+}
+
+def checkout() {
+	checkout([
+		$class: 'GitSCM', 
+		branches: [[name: gitBranch ]], 
+		doGenerateSubmoduleConfigurations: false, 
+		clearWorkspace: true,
+		extensions: [
+			[$class: 'CleanCheckout'], [
+			$class: 'SubmoduleOption', 
+			disableSubmodules: false, 
+			parentCredentials: true, 
+			recursiveSubmodules: true, 
+			reference: '', trackingSubmodules: false]], 
+		submoduleCfg: [], 
+		userRemoteConfigs: [[credentialsId: gitCreds, url: gitRepo]]
+	])
+}
+
+def set_env_variables() {
+	env.TF_VAR_db_engine            = "${db_engine}"
+	env.TF_VAR_db_family            = "${db_family}"
+	env.TF_VAR_db_engine_version    = "${db_engine_version}"
+	env.TF_VAR_db_instance_class    = "${db_instance_class}"
+	env.TF_VAR_db_identifier        = "${db_identifier}"
+	env.TF_VAR_db_name              = "${db_name}"
+	env.TF_VAR_db_username          = "${db_username}"
+	env.TF_VAR_db_allocated_storage = "${db_allocated_storage}"
+	env.TF_VAR_db_multi_az          = "${db_multi_az}"
+	env.TF_VAR_db_R53_name          = "${db_R53_name}"
+}
+
+def terraform_init() {
+	withEnv(["GIT_ASKPASS=${WORKSPACE}/askp-${BUILD_TAG}"]){
+		withCredentials([usernamePassword(credentialsId: gitCreds, usernameVariable: 'STASH_USERNAME', passwordVariable: 'STASH_PASSWORD')]) {
+			sh "terraform init -no-color -input=false -upgrade=true -backend=true -force-copy -backend-config='bucket=${tfstateBucket}' -backend-config='key=${tfstateBucketPrefix}/${db_identifier}-${db_engine}-rds.tfstate'"
+		}
+	}
+}
+
+def terraform_plan(global_tfvars,rds_tfvars) {
+	sh "terraform plan -no-color -out=tfplan -input=false -var-file=${global_tfvars} -var-file=${rds_tfvars}"
+}
+
+def terraform_apply() {
+	sh "terraform apply -no-color -input=false tfplan"
+}
+
+def terraform_plan_destroy(global_tfvars,rds_tfvars) {
+        sh "terraform plan -destroy -no-color -out=tfdestroy -input=false -var-file=${global_tfvars} -var-file=${rds_tfvars}"
+}
+
+def terraform_destroy() {
+        sh "terraform apply -no-color -input=false tfdestroy"
+}
