bbabdbf3f53a8885022309bd3fea94e05fdbc7e7
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-08-29 01:56:09 +0530

    Update all_terraform_jobs_groovy.groovy

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -4,8 +4,8 @@ def gitCreds            		= "GitCred"
 def terraformTFstateBucket		= "terraform-tfstate-mumbai"
 def terraformTFstateBucketPrefix	= "rds_module"
 
-// RDS Build Generic Job
-pipelineJob('tf-1-rds-build-generic-job') {
+// RDS DB Build Generic Job
+pipelineJob('tf-1-rds-db-build-generic-job') {
         description('Explains how to use Jenins Approval for Build Jobs')
         logRotator(-1,-1)
         parameters{
@@ -31,7 +31,40 @@ pipelineJob('tf-1-rds-build-generic-job') {
         }
         definition {
                 cps {
-                        script(readFileFromWorkspace('pipeline/tf-1-rds-build-generic.groovy'))
+                        script(readFileFromWorkspace('pipeline/tf-1-rds-db-build-generic.groovy'))
+                        sandbox()
+                }
+        }
+}
+
+// RDS Cluster Build Generic Job
+pipelineJob('tf-1-rds-cluster-build-generic-job') {
+        description('Explains how to use Jenins Approval for Build Jobs')
+        logRotator(-1,-1)
+        parameters{
+                choiceParam('gitRepo'                   , [terraformRepo]       	, '')
+                choiceParam('gitBranch'                 , [terraformBranch]     	, '')
+                choiceParam('gitCreds'                  , [gitCreds]            	, '')
+                choiceParam('tfstateBucket'             , [terraformTFstateBucket]      , 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [terraformTFstateBucketPrefix], 'TF State Bucket Prefix'      )
+		stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
+		stringParam('db_engine'                 , 'postgres,oracle-se1'		, '')
+                stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'		, '')
+                choiceParam('db_instance_class'         , ['db.t2.small']       	, '')
+                stringParam('db_identifier'             , 'test-instance'       	, '')
+                choiceParam('db_name'                   , ['DBNAME']			, '')
+                choiceParam('db_username'               , ['Administrator']     	, '')
+                nonStoredPasswordParam('db_password'    , 'Do you think that you can see !!')
+                choiceParam('db_allocated_storage'      , ['10']			, 'in GBs')
+                choiceParam('db_multi_az'               , ['false','true']      	, '')
+                choiceParam('createInstance'            , ['true','false']      	, '')
+                stringParam('db_R53_name'               , 'R53_Name'            	, '')
+                choiceParam('createInstanceDNS'         , ['true','false']      	, '')
+                choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
+        }
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/tf-1-rds-cluster-build-generic.groovy'))
                         sandbox()
                 }
         }
