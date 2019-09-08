fcc650ae71206ad1b4cf1624b03b0c768c7f1155
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-09-08 11:31:45 +0530

    Update all_terraform_jobs_groovy.groovy

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -1,19 +1,20 @@
 def terraformRepo       		= "https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
 def terraformBranch     		= "master"
 def gitCreds            		= "GitCred"
-def terraformTFstateBucket		= "terraform-tfstate-mumbai"
-def terraformTFstateBucketPrefix	= "rds_module"
+def tfStateBucket			= "terraform-tfstate-mumbai"
+def tfStateBucketPrefixRDS		= "rds_module"
+def tfStateBucketPrefixR53		= "r53_module"
 
 // RDS DB Build Generic Job
-pipelineJob('tf-1-rds-db-build-generic-job') {
+pipelineJob('tf-1-rds-db-build-job') {
         description('Explains how to use Jenins Approval for Build Jobs')
         logRotator(-1,-1)
         parameters{
                 choiceParam('gitRepo'                   , [terraformRepo]       	, '')
                 choiceParam('gitBranch'                 , [terraformBranch]     	, '')
                 choiceParam('gitCreds'                  , [gitCreds]            	, '')
-                choiceParam('tfstateBucket'             , [terraformTFstateBucket]      , 'TF State Bucket'             )
-                choiceParam('tfstateBucketPrefix'       , [terraformTFstateBucketPrefix], 'TF State Bucket Prefix'      )
+                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixRDS]	, 'TF State Bucket Prefix'      )
 		stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
 		stringParam('db_engine'                 , 'postgres,oracle-se1'		, '')
                 stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'		, '')
@@ -31,40 +32,29 @@ pipelineJob('tf-1-rds-db-build-generic-job') {
         }
         definition {
                 cps {
-                        script(readFileFromWorkspace('pipeline/tf-1-rds-db-build-generic.groovy'))
+                        script(readFileFromWorkspace('pipeline/tf-1-rds-db-build.groovy'))
                         sandbox()
                 }
         }
 }
 
-// RDS Cluster Build Generic Job
-pipelineJob('tf-1-rds-cluster-build-generic-job') {
+// Route53 Zone Creation
+pipelineJob('tf-1-route53-zone-build-job') {
         description('Explains how to use Jenins Approval for Build Jobs')
         logRotator(-1,-1)
         parameters{
                 choiceParam('gitRepo'                   , [terraformRepo]       	, '')
                 choiceParam('gitBranch'                 , [terraformBranch]     	, '')
                 choiceParam('gitCreds'                  , [gitCreds]            	, '')
-                choiceParam('tfstateBucket'             , [terraformTFstateBucket]      , 'TF State Bucket'             )
-                choiceParam('tfstateBucketPrefix'       , [terraformTFstateBucketPrefix], 'TF State Bucket Prefix'      )
-		stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
-		stringParam('db_engine'                 , 'postgres,oracle-se1'		, '')
-                stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'		, '')
-                choiceParam('db_instance_class'         , ['db.t2.small']       	, '')
-                stringParam('db_identifier'             , 'test-instance'       	, '')
-                choiceParam('db_name'                   , ['DBNAME']			, '')
-                choiceParam('db_username'               , ['Administrator']     	, '')
-                nonStoredPasswordParam('db_password'    , 'Do you think that you can see !!')
-                choiceParam('db_allocated_storage'      , ['10']			, 'in GBs')
-                choiceParam('db_multi_az'               , ['false','true']      	, '')
-                choiceParam('createInstance'            , ['true','false']      	, '')
-                stringParam('db_R53_name'               , 'R53_Name'            	, '')
-                choiceParam('createInstanceDNS'         , ['true','false']      	, '')
-                choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
-        }
+                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixR53]	, 'TF State Bucket Prefix'      )
+		stringParam('r53_zone_name'		, '', '')
+		stringParam('r53_zone_description'	, '', 'Private | Public + r53_zone_name')
+                stringParam('vpc_name'			, '', '')
+	}
         definition {
                 cps {
-                        script(readFileFromWorkspace('pipeline/tf-1-rds-cluster-build-generic.groovy'))
+                        script(readFileFromWorkspace('pipeline/tf-1-route53-zone-build.groovy'))
                         sandbox()
                 }
         }
