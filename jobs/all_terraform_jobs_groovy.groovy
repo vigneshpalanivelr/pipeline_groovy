a740259d54b491ccdadcae3ae0e6daa32857c2ed
vigneshpalanivelr <vigneshpalanivelr@gmail.com>
2019-09-08 07:13:50 +0000

    Created Groovy: R53 & R53-ac Pipeline ;; Updated Groovy: RDS Pipeline

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -4,17 +4,20 @@ def gitCreds            		= "GitCred"
 def tfStateBucket			= "terraform-tfstate-mumbai"
 def tfStateBucketPrefixRDS		= "rds_module"
 def tfStateBucketPrefixR53		= "r53_module"
+def tfStateBucketPrefixR53ac		= "r53ac_module"
+def tfStateBucketPrefixKMS		= "kms_module"
 
 // RDS DB Build Generic Job
 pipelineJob('tf-1-rds-db-build-job') {
-        description('Explains how to use Jenins Approval for Build Jobs')
+        description('Building AWS RDS (PostgreSQL | Oracle | MySql | MariaDb)')
         logRotator(-1,-1)
         parameters{
                 choiceParam('gitRepo'                   , [terraformRepo]       	, '')
                 choiceParam('gitBranch'                 , [terraformBranch]     	, '')
                 choiceParam('gitCreds'                  , [gitCreds]            	, '')
-                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
-                choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixRDS]	, 'TF State Bucket Prefix'      )
+                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             	)
+                choiceParam('tfstateBucketPrefixRDS'	, [tfStateBucketPrefixRDS]	, 'TF State Bucket Prefix - RDS'	)
+		choiceParam('tfstateBucketPrefixDNS'    , [tfStateBucketPrefixR53ac]	, 'TF State Bucket Prefix - DNS'	)
 		stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
 		stringParam('db_engine'                 , 'postgres,oracle-se1'		, '')
                 stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'		, '')
@@ -25,9 +28,10 @@ pipelineJob('tf-1-rds-db-build-job') {
                 nonStoredPasswordParam('db_password'    , 'Do you think that you can see !!')
                 choiceParam('db_allocated_storage'      , ['10']			, 'in GBs')
                 choiceParam('db_multi_az'               , ['false','true']      	, '')
-                choiceParam('createInstance'            , ['true','false']      	, '')
-                stringParam('db_R53_name'               , 'R53_Name'            	, '')
-                choiceParam('createInstanceDNS'         , ['true','false']      	, '')
+		choiceParam('db_read_replica'		, ['true','false']      	, '')
+                choiceParam('db_apply_changes'		, ['true','false']      	, '')
+		choiceParam('includeInstance'		, ['true','false']      	, '')
+                choiceParam('includeInstanceDNS'	, ['false','true']      	, '')
                 choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
         }
         definition {
@@ -40,7 +44,7 @@ pipelineJob('tf-1-rds-db-build-job') {
 
 // Route53 Zone Creation
 pipelineJob('tf-1-route53-zone-build-job') {
-        description('Explains how to use Jenins Approval for Build Jobs')
+        description('Building AWS Route53 Zone Creation')
         logRotator(-1,-1)
         parameters{
                 choiceParam('gitRepo'                   , [terraformRepo]       	, '')
@@ -48,14 +52,61 @@ pipelineJob('tf-1-route53-zone-build-job') {
                 choiceParam('gitCreds'                  , [gitCreds]            	, '')
                 choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
                 choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixR53]	, 'TF State Bucket Prefix'      )
-		stringParam('r53_zone_name'		, '', '')
-		stringParam('r53_zone_description'	, '', 'Private | Public + r53_zone_name')
-                stringParam('vpc_name'			, '', '')
+		stringParam('r53_zone_name'		, 'vignesh-private.zone.com'	, '')
+                stringParam('vpc_name'			, 'Default_VPC'			, '')
+		choiceParam('includeR53Zone'		, ['true','false']      	, '')
 		choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
 	}
         definition {
                 cps {
                         script(readFileFromWorkspace('pipeline/tf-1-route53-zone-build.groovy'))
+                        sandbox()
+                }
+        }
+}
+
+// Route53 A-record and CNAME Creation
+pipelineJob('tf-1-route53ac-record-build-job') {
+        description('Building AWS Route53 Record Creation')
+        logRotator(-1,-1)
+        parameters{
+                choiceParam('gitRepo'                   , [terraformRepo]       	, '')
+                choiceParam('gitBranch'                 , [terraformBranch]     	, '')
+                choiceParam('gitCreds'                  , [gitCreds]            	, '')
+                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixR53ac]	, 'TF State Bucket Prefix'      )
+		stringParam('r53_zone_name'		, 'vignesh-private-zone'	, 'zone name')
+		stringParam('r53_record_name'		, 'postgres-r53,ec2-r53'	, 'route53 name')
+		stringParam('r53_records'		, ''				, 'ip-address | end-point')
+		choiceParam('r53_record_type'		, ['A','CNAME']      		, 'A : ip-address | CNAME : end-point')
+		choiceParam('includeR53acRecord'	, ['true','false']      	, '')
+		choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
+	}
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/tf-1-route53ac-record-build.groovy'))
+                        sandbox()
+                }
+        }
+}
+
+// AWS KMS Key Creation
+pipelineJob('tf-1-kms-key-build-job') {
+        description('Building AWS KMS key creation')
+        logRotator(-1,-1)
+        parameters{
+                choiceParam('gitRepo'                   , [terraformRepo]       	, '')
+                choiceParam('gitBranch'                 , [terraformBranch]     	, '')
+                choiceParam('gitCreds'                  , [gitCreds]            	, '')
+                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             )
+                choiceParam('tfstateBucketPrefix'       , [tfStateBucketPrefixKMS]	, 'TF State Bucket Prefix'      )
+		stringParam('kms_key_name'		, 'custome-key'			, '')
+		choiceParam('includeKMSKey'		, ['true','false']      	, '')
+		choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '')
+	}
+        definition {
+                cps {
+                        script(readFileFromWorkspace('pipeline/tf-1-kms-key-build.groovy'))
                         sandbox()
                 }
         }
--- pipeline/tf-1-rds-db-build.groovy
+++ pipeline/tf-1-rds-db-build.groovy
@@ -16,31 +16,53 @@
 *	db_allocated_storage
 *	db_multi_az
 *	db_R53_name
+*	db_read_replica
+*	db_apply_changes
 
-*	createInstance
-*	createInstanceDNS
+*	includeInstance
+*	includeInstanceDNS
 *	terraformApplyPlan
 
-Error
+Error on pipeline
 	local variable	: Use def keyword
 	global variable	: No def keyword
-	
 	sqlserver-ex	: Engine sqlserver-ex does not support encryption at rest
 	sqlserver-web 	: DBName must be null for engine
 	sqlserver-se	: DBName must be null for engine
+
+Error on Terraform
+	Creating	: DBSubnetGroupNotAllowedFault: DbSubnetGroupName should not be specified for read replicas that are created in the same region as the master
+	Creating	: Cannot change master user password on an RDS postgres Read Replica because it uses physical replication and therefore cannot differ from its parent.
+	Destroying 	: DB Instance FinalSnapshotIdentifier is required when a final snapshot is required
+
+Steps
+	1)	Create RDS Instance
+	2)	Create RDS DNS
+	3)	Destroy RDS DNS
+	4)	Destroy RDS Instance
+	5)	Defined all the required functions
+
+Pending Implementation
+	1)	Creating CW Alarms
+	2)	Creating Application DBA user
 */
 
 node('master') {
 
-	terraformDirectoryRDS	= "modules/all_modules/${tfstateBucketPrefix}"
+	terraformDirectoryRDS	= "modules/all_modules/rds_module"
+	terraformDirectoryDNS	= "modules/all_modules/rds_dns_module"
+
 	global_tfvars   	= "../../../variables/global_vars.tfvars"
-	rds_tfvars      	= "../../../variables/rds.tfvars"
+	rds_tfvars      	= "../../../variables/rds_vars.tfvars"
+	rds_dns_tfvars		= "../../../variables/rds_dns_vars.tfvars"
+
 	db_rds 			= (db_engine		=~ /[a-zA-Z]+/)[0]
 	db_engine_major_version = (db_engine_version	=~ /\d+.\d+/)[0]
+	
 	date 			= new Date()
-
 	println date
 	
+
 	writeFile(file: "askp-${BUILD_TAG}",text:"#!/bin/bash\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) \"\${STASH_PASSWORD}\" ;;\nesac")
 	sh "chmod a+x askp-${BUILD_TAG}"
 	
@@ -49,58 +71,111 @@ node('master') {
 	}
 	stage('Checkout') {
 		checkout()
-		if (createInstance == 'true'){
-			dir(terraformDirectoryRDS){
-				stage('Remote State Init') {
-					terraform_init()
+		//Create RDS Instance
+		if ((includeInstance == 'true') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
+			dir(terraformDirectoryRDS) {
+				stage('RDS Init') {
+					terraform_rds_init()
 				}
 				if (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply') {
-					stage('Terraform Plan'){
-						withEnv(["TF_VAR_db_password=${db_password}"]){
+					stage('RDS Plan'){
+						withEnv(["TF_VAR_db_password=${db_password}"]) {
 							set_env_variables()
-                		               		terraform_plan(global_tfvars,rds_tfvars)
+							terraform_rds_plan(global_tfvars,rds_tfvars)
 						}
 					}
 				}
 				if (terraformApplyPlan == 'apply') {
-					stage('Approve Plan'){
+					stage('RDS Approve'){
 						approval()
 					}
-					stage('Terraform Apply'){
-						terraform_apply()
+					stage('RDS Apply') {
+						terraform_rds_apply()
 					}
 				}
+			}
+		}
+		//Create RDS DNS
+		if ((includeInstanceDNS == 'true') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
+			dir(terraformDirectoryDNS) {
+				stage('DNS Init') {
+					terraform_dns_init()
+				}
+				if (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply') {
+					stage('DNS Plan') {
+						set_env_variables()
+						terraform_dns_plan(global_tfvars,rds_dns_tfvars)
+					}
+				}
+				if (terraformApplyPlan == 'apply') {
+					stage('DNS Approve'){
+						approval()
+					}
+					stage('DNS Apply') {
+						terraform_dns_apply()
+					}
+				}
+			}
+		}
+		//Destroy RDS DNS
+		if ((includeInstanceDNS == 'true') && (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy')) {
+			dir(terraformDirectoryDNS) {
+				stage('DNS Init') {
+					terraform_dns_init()
+				}
 				if (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy') {
-					stage('Plan Destroy'){
-                                                withEnv(["TF_VAR_db_password=${db_password}"]){
+					stage('DNS Destroy Plan') {
+						set_env_variables()
+						terraform_dns_plan_destroy(global_tfvars,rds_dns_tfvars)
+					}
+				}
+				if (terraformApplyPlan == 'destroy') {
+					stage('DNS Approve') {
+						approval()
+					}
+					stage('DNS Destroy') {
+						terraform_dns_destroy()
+					}
+				}
+			}
+		}
+		//Destroy RDS Instance
+		if ((includeInstance == 'true') && (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy')) {
+			dir(terraformDirectoryRDS) {
+				stage('RDS Init') {
+					terraform_rds_init()
+				}
+				if (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy') {
+					stage('RDS Destroy Plan') {
+						set_env_variables()
+						withEnv(["TF_VAR_db_password=${db_password}"]) {
 							set_env_variables()
-	        	                        	terraform_plan_destroy(global_tfvars,rds_tfvars)
+							terraform_rds_plan_destroy(global_tfvars,rds_tfvars)
 						}
 					}
 				}
 				if (terraformApplyPlan == 'destroy') {
-					stage('Approve Destroy'){
-                                                approval()
-                                	}
-					stage(' Destroy'){
-                                                terraform_destroy()
-                                	}
+					stage('RDS Approve') {
+						approval()
+					}
+					stage('RDS Destroy') {
+						terraform_rds_destroy()
+					}
 				}
 			}
 		}
 	}
 }
 
+//Common functions
 def approval() {
-	timeout(time: 15, unit: 'SECONDS') {
+	timeout(time: 1, unit: 'MINUTES') {
 		input(
 			id: 'Approval', message: 'Shall I Continue ?', parameters: [[
-				$class:	'BooleanParameterDefinition', defaultValue: true, description: 'default to tick', name: 'Please confirm to proceed'
-			]]
+				$class:	'BooleanParameterDefinition', defaultValue: true, description: 'default to tick', name: 'Please confirm to proceed']]
 		)
 	}
 }
-
 def checkout() {
 	checkout([
 		$class: 'GitSCM', 
@@ -118,42 +193,60 @@ def checkout() {
 		userRemoteConfigs: [[credentialsId: gitCreds, url: gitRepo]]
 	])
 }
-
 def set_env_variables() {
 	env.TF_VAR_db_family            	= "${db_family}"
 	env.TF_VAR_db_engine            	= "${db_engine}"
 	env.TF_VAR_db_engine_version    	= "${db_engine_version}"
-	env.TF_VAR_db_instance_class    	= "${db_instance_class}"
 	env.TF_VAR_db_identifier        	= "${db_identifier}"
+	env.TF_VAR_db_instance_class    	= "${db_instance_class}"
+	env.TF_VAR_db_rds			= "${db_rds}"
 	env.TF_VAR_db_name              	= "${db_name}"
 	env.TF_VAR_db_username          	= "${db_username}"
 	env.TF_VAR_db_allocated_storage 	= "${db_allocated_storage}"
 	env.TF_VAR_db_multi_az          	= "${db_multi_az}"
-	env.TF_VAR_db_R53_name          	= "${db_R53_name}"
-	env.TF_VAR_db_rds			= "${db_rds}"
 	env.TF_VAR_db_engine_major_version	= "${db_engine_major_version}"
+	env.TF_VAR_db_read_replica		= "${db_read_replica}"
+	env.TF_VAR_db_apply_immediately		= "${db_apply_changes}"
 }
 
-def terraform_init() {
+//RDS Instance creation-destroy functions
+def terraform_rds_init() {
 	withEnv(["GIT_ASKPASS=${WORKSPACE}/askp-${BUILD_TAG}"]){
 		withCredentials([usernamePassword(credentialsId: gitCreds, usernameVariable: 'STASH_USERNAME', passwordVariable: 'STASH_PASSWORD')]) {
-			sh "terraform init -no-color -input=false -upgrade=true -backend=true -force-copy -backend-config='bucket=${tfstateBucket}' -backend-config='key=${tfstateBucketPrefix}/${db_identifier}-${db_rds}-rds.tfstate'"
+			sh "terraform init -no-color -input=false -upgrade=true -backend=true -force-copy -backend-config='bucket=${tfstateBucket}' -backend-config='key=${tfstateBucketPrefixRDS}/${db_identifier}-${db_rds}-rds.tfstate'"
 		}
 	}
 }
-
-def terraform_plan(global_tfvars,rds_tfvars) {
+def terraform_rds_plan(global_tfvars,rds_tfvars) {
 	sh "terraform plan -no-color -out=tfplan -input=false -var-file=${global_tfvars} -var-file=${rds_tfvars}"
 }
-
-def terraform_apply() {
+def terraform_rds_apply() {
 	sh "terraform apply -no-color -input=false tfplan"
 }
-
-def terraform_plan_destroy(global_tfvars,rds_tfvars) {
-        sh "terraform plan -destroy -no-color -out=tfdestroy -input=false -var-file=${global_tfvars} -var-file=${rds_tfvars}"
+def terraform_rds_plan_destroy(global_tfvars,rds_tfvars) {
+	sh "terraform plan -destroy -no-color -out=tfdestroy -input=false -var-file=${global_tfvars} -var-file=${rds_tfvars}"
+}
+def terraform_rds_destroy() {
+	sh "terraform apply -no-color -input=false tfdestroy"
 }
 
-def terraform_destroy() {
-        sh "terraform apply -no-color -input=false tfdestroy"
+//RDS DNS creation/destroy functions
+def terraform_dns_init() {
+	withEnv(["GIT_ASKPASS=${WORKSPACE}/askp-${BUILD_TAG}"]){
+		withCredentials([usernamePassword(credentialsId: gitCreds, usernameVariable: 'STASH_USERNAME', passwordVariable: 'STASH_PASSWORD')]) {
+			sh "terraform init -no-color -input=false -upgrade=true -backend=true -force-copy -backend-config='bucket=${tfstateBucket}' -backend-config='key=${tfstateBucketPrefixDNS}/${db_identifier}-${db_rds}-dns.tfstate'"
+		}
+	}
+}
+def terraform_dns_plan(global_tfvars,rds_dns_tfvars) {
+	sh "terraform plan -no-color -out=tfplan -input=false -var-file=${global_tfvars} -var-file=${rds_dns_tfvars}"
+}
+def terraform_dns_apply() {
+	sh "terraform apply -no-color -input=false tfplan"
+}
+def terraform_dns_plan_destroy(global_tfvars,rds_dns_tfvars) {
+	sh "terraform plan -destroy -no-color -out=tfdestroy -input=false -var-file=${global_tfvars} -var-file=${rds_dns_tfvars}"
+}
+def terraform_dns_destroy() {
+	sh "terraform apply -no-color -input=false tfdestroy"
 }
