7baef5f7116abd033d143f0ff7218b195bc90175
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-09-15 10:01:09 +0530

    Update all_terraform_jobs_groovy.groovy

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -1,10 +1,10 @@
 def terraformRepo       		= "https://github.com/vigneshpalanivelr/terraform_practice_codes.git"
 def terraformBranch     		= "master"
 def gitCreds            		= "GitCred"
-def tfStateBucket			= "terraform-tfstate-mumbai"
+def tfStateBucket				= "terraform-tfstate-mumbai"
 def tfStateBucketPrefixRDS		= "rds_module"
 def tfStateBucketPrefixR53		= "r53_module"
-def tfStateBucketPrefixR53ac		= "r53ac_module"
+def tfStateBucketPrefixR53ac	= "r53ac_module"
 def tfStateBucketPrefixKMS		= "kms_module"
 
 // RDS DB Build Generic Job
@@ -23,34 +23,36 @@ pipelineJob('tf-1-rds-db-build-job') {
 		    ''')
         logRotator(-1,-1)
         parameters{
-                choiceParam('gitRepo'                   , [terraformRepo]       	, '')
-                choiceParam('gitBranch'                 , [terraformBranch]     	, '')
-                choiceParam('gitCreds'                  , [gitCreds]            	, '')
-                choiceParam('tfstateBucket'             , [tfStateBucket]      		, 'TF State Bucket'             	)
-                choiceParam('tfstateBucketPrefixRDS'	, [tfStateBucketPrefixRDS]	, 'TF State Bucket Prefix - RDS'	)
-		choiceParam('tfstateBucketPrefixDNS'    , [tfStateBucketPrefixR53ac]	, 'TF State Bucket Prefix - DNS'	)
-		stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
-		stringParam('db_engine'                 , 'postgres,oracle-se1'		, '')
-                stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'		, '')
-                choiceParam('db_instance_class'         , ['db.t2.small']       	, '')
-                stringParam('db_identifier'             , 'test-instance'       	, '''TF-STATE : Statefile for Instance<br>
-			    db_identifier.tfstate''')
-                choiceParam('db_name'                   , ['DBNAME']			, '')
-                choiceParam('db_username'               , ['Administrator']     	, '')
+                choiceParam('gitRepo'                   , [terraformRepo]       		, '')
+                choiceParam('gitBranch'                 , [terraformBranch]     		, '')
+                choiceParam('gitCreds'                  , [gitCreds]            		, '')
+                choiceParam('tfstateBucket'             , [tfStateBucket]      			, 'TF State Bucket'             	)
+                choiceParam('tfstateBucketPrefixRDS'	, [tfStateBucketPrefixRDS]		, 'TF State Bucket Prefix - RDS'	)
+				choiceParam('tfstateBucketPrefixDNS'    , [tfStateBucketPrefixR53ac]	, 'TF State Bucket Prefix - DNS')
+				stringParam('db_family'                 , 'postgres9.6,oracle-se1-11.2'	, '')
+				stringParam('db_engine'                 , 'postgres,oracle-se1'			, '')
+                stringParam('db_engine_version'         , '9.6.11,11.2.0.4.v21'			, '')
+                choiceParam('db_instance_class'         , ['db.t2.small']       		, '')
+                stringParam('db_identifier'             , 'test-instance'       		, '''TF-STATE : Statefile for Instance<br>
+					db_identifier.tfstate''')
+                choiceParam('db_name'                   , ['DBNAME']					, '')
+                choiceParam('db_username'               , ['Administrator']     		, '')
                 nonStoredPasswordParam('db_password'    , 'Do you think that you can see !!')
-                choiceParam('db_allocated_storage'      , ['10']			, 'in GBs')
-                choiceParam('db_multi_az'               , ['false','true']      	, '')
-		choiceParam('db_read_replica'		, ['true','false']      	, '')
-                choiceParam('db_apply_changes'		, ['true','false']      	, '')
-		choiceParam('includeInstance'		, ['true','false']      	, '')
-		stringParam('db_route53_name'		, 'test-instance'		, '''TF-STATE : Statefile for Route53 Name<br>
-			    db_route53_name-dns.tfstate''')
-                choiceParam('includeInstanceDNS'	, ['false','true']      	, '')
+                choiceParam('db_allocated_storage'      , ['10']						, 'in GBs')
+                choiceParam('db_multi_az'               , ['false','true']      		, '')
+				choiceParam('db_read_replica'			, ['true','false']      		, '')
+                choiceParam('db_apply_changes'			, ['true','false']      		, '')
+				choiceParam('includeMaster'				, ['true','false']      		, '')
+				stringParam('source_identifier'			, 'test-instance'       		, 'source instance to replicate')
+				choiceParam('includeReplica'			, ['true','false']      		, '')
+				stringParam('db_route53_name'			, 'test-instance'				, '''TF-STATE : Statefile for Route53 Name<br>
+					db_route53_name-dns.tfstate''')
+                choiceParam('includeInstanceDNS'		, ['false','true']      		, '')
                 choiceParam('terraformApplyPlan'        , ['plan','apply','plan-destroy','destroy']	, '''
-			    <br>&emsp plan&emsp&emsp&emsp&emsp: only plan to create 
-			    <br>&emsp apply&emsp&emsp&emsp&ensp: will apply above plan 
-			    <br>&emsp plan-destroy&nbsp&nbsp: only plan to destroy
-			    <br>&emsp destroy&emsp&emsp&ensp&nbsp: will apply above plan-destroy''') 
+					<br>&emsp plan&emsp&emsp&emsp&emsp: only plan to create 
+					<br>&emsp apply&emsp&emsp&emsp&ensp: will apply above plan 
+					<br>&emsp plan-destroy&nbsp&nbsp: only plan to destroy
+					<br>&emsp destroy&emsp&emsp&ensp&nbsp: will apply above plan-destroy''') 
         }
         definition {
                 cps {
