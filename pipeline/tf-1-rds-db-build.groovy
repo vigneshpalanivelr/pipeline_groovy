6680ea5c851e5ce86ce1dc864cd23137c20a0eec
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2019-09-16 00:20:47 +0530

    Update tf-1-rds-db-build.groovy

--- pipeline/tf-1-rds-db-build.groovy
+++ pipeline/tf-1-rds-db-build.groovy
@@ -74,7 +74,7 @@ node('master') {
 	stage('Checkout') {
 		checkout()
 		//Create Master RDS Instance
-		if ((db_action == 'master' || db_action == 'promote-as-master') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
+		if ((db_action == 'master') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
 			dir(terraformDirMasterRDS) {
 				stage('RDS Init') {
 					terraform_master_init()
@@ -98,7 +98,7 @@ node('master') {
 			}
 		}
 		//Create Replica RDS Instance
-		if ((db_action == 'replica' || db_action == 'promote') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
+		if ((db_action != 'master') && (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply')) {
 			dir(terraformDirReplicaRDS) {
 				stage('RDS Init') {
 					terraform_master_init()
