83a09fd5b3bd78ccfa3593919ecf03820c381c06
root <root@ip-172-31-47-58.ap-south-1.compute.internal>
2020-01-10 08:03:59 +0000

    Added Pipeline Groovy

--- admin-pipeline/admin_view.groovy
+++ admin-pipeline/admin_view.groovy
@@ -1,19 +1,21 @@
 nestedView('seed-jobs') {
 	views {
 		listView('seed-jobs') {
-			description('All Seed Jobs')
-			jobs {
-//				name('admin-seed-job')
-				regex('/admin-*')
-			}
-			columns {
-				status()
-				weather()
-				name()
-				lastSuccess()
-				lastFailure()
-				lastDuration()
-				buildButton()
+			views {
+				description('All Seed Jobs')
+				jobs {
+//					name('admin-seed-job')
+					regex('/admin-*')
+				}
+				columns {
+					status()
+					weather()
+					name()
+					lastSuccess()
+					lastFailure()
+					lastDuration()
+					buildButton()
+				}
 			}
 		}
 	}
