c7081cf5bd3475481f1415ebb75c1b9aefc1ed00
root <root@ip-172-31-47-58.ap-south-1.compute.internal>
2020-01-10 10:28:52 +0000

    Added few seed job

--- admin-pipeline/admin_seed_job.groovy
+++ admin-pipeline/admin_seed_job.groovy
@@ -1,14 +1,19 @@
 node('master') {
-	stage('Clone Repository') {
-		checkout([
-			$class					: 'GitSCM',
-			branches				: [[name: jobDSL_branch]],
-			doGenerateSubmoduleConfigurations	: false,
-			extensions				: [],
-			submoduleCfg				: [],
-			userRemoteConfigs			: [[credentialsId: jobDSL_creds, url: jobDSL_repo]]
-		])
-	}
+        stage('Clone & Checkout') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: jobDSL_branch ]],
+                        doGenerateSubmoduleConfigurations: false,
+                        clearWorkspace: true,
+                        extensions: [[$class: 'CleanCheckout'], [
+                                $class: 'SubmoduleOption',
+                                disableSubmodules: false,
+                                parentCredentials: true,
+                                recursiveSubmodules: true,
+                                reference: '', trackingSubmodules: false]],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[credentialsId: jobDSL_creds, url: jobDSL_repo]]])
+        }
 	stage('Process DSL') {
 		jobDsl targets : jobDSL_path
 	}
