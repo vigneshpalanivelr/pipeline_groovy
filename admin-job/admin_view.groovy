ed358674eab60f217dded8fd76f895cfc10f0eb0
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-01-25 00:45:28 +0530

    Created Groovy: EBS Pipeline

--- admin-job/admin_view.groovy
+++ admin-job/admin_view.groovy
@@ -1,43 +1,43 @@
 nestedView('seed-jobs') {
-        views {
-                listView('seed-jobs') {
-                        views {
-                                description('All Seed Jobs')
-                                jobs {
-                                        regex(/admin.*/)
-                                }
-                                columns {
-                                        status()
-                                        weather()
-                                        name()
-                                        lastSuccess()
-                                        lastFailure()
-                                        lastDuration()
-                                        buildButton()
-                                }
-                        }
-                }
-        }
+	views {
+		listView('seed-jobs') {
+			views {
+				description('All Seed Jobs')
+				jobs {
+					regex(/admin.*/)
+				}
+				columns {
+					status()
+					weather()
+					name()
+					lastSuccess()
+					lastFailure()
+					lastDuration()
+					buildButton()
+				}
+			}
+		}
+	}
 }
 nestedView('terraform-jobs') {
-        views {
-                listView('rds') {
-                        views {
-                                description('All Terraform Jobs')
-                                jobs {
-                                        regex(/tf-rds-.*/)
-                                }
-                                columns {
-                                        status()
-                                        weather()
-                                        name()
-                                        lastSuccess()
-                                        lastFailure()
-                                        lastDuration()
-                                        buildButton()
-                                }
-                        }
-                }
+	views {
+		listView('rds') {
+			views {
+				description('All Terraform Jobs')
+				jobs {
+					regex(/tf-rds-.*/)
+				}
+				columns {
+					status()
+					weather()
+					name()
+					lastSuccess()
+					lastFailure()
+					lastDuration()
+					buildButton()
+				}
+			}
+		}
 		listView('r53') {
 			views {
 				description('All Terraform Jobs')
@@ -89,6 +89,23 @@ nestedView('terraform-jobs') {
 				}
 			}
 		}
+		listView('ebs') {
+			views {
+				description('All Terraform Jobs')
+				jobs {
+					regex(/tf-ebs.*/)
+				}
+				columns {
+					status()
+					weather()
+					name()
+					lastSuccess()
+					lastFailure()
+					lastDuration()
+					buildButton()
+				}
+			}
+		}
 	}
 }
 nestedView('practice-jobs') {
@@ -110,26 +127,26 @@ nestedView('practice-jobs') {
 				}
 			}
 		}
-        }
+	}
 }
 nestedView('master-jobs') {
-        views {
-                listView('master-jobs') {
-                        views {
-                                description('Master Jobs')
-                                jobs {
-                                        regex(/master-.*job/)
-                                }
-                                columns {
-                                        status()
-                                        weather()
-                                        name()
-                                        lastSuccess()
-                                        lastFailure()
-                                        lastDuration()
-                                        buildButton()
-                                }
-                        }
-                }
-        }
+	views {
+		listView('master-jobs') {
+			views {
+				description('Master Jobs')
+				jobs {
+					regex(/master-.*job/)
+				}
+				columns {
+					status()
+					weather()
+					name()
+					lastSuccess()
+					lastFailure()
+					lastDuration()
+					buildButton()
+				}
+			}
+		}
+	}
 }
