693efd2f3ef3000890f0310e56d9fdb8ecdcbbf4
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-01-28 21:17:26 +0530

    Added playbook-provisioning-job

---
+++ jobs/all_scripts.groovy
@@ -0,0 +1,20 @@
+def scriptsRepo     = "https://github.com/vigneshpalanivelr/all_scripts.git"
+def scriptsBranch   = "master"
+def gitCreds        = "gitCreds"
+
+// RDS DB Build Generic Job
+pipelineJob('playbook-provisioning-job') {
+    logRotator (-1,-1)
+	parameters {
+        choiceParam('gitRepo'					, [terraformRepo]				, '')
+		choiceParam('gitBranch'					, [terraformBranch]				, '')
+		choiceParam('gitCreds'					, [gitCreds]					, '')
+        choiceParam('scriptType'				, ['select','ansible','python','pgsql']					, '')
+    }
+    definition {
+		cps {
+			script(readFileFromWorkspace('pipeline/playbook-provisioning.groovy'))
+			sandbox()
+		}
+	}
+}
