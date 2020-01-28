db49e6ce7a35d1264eff1d2d8f18c210ad990887
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-01-28 22:11:51 +0530

    Update playbook-provisioning.groovy

--- pipeline/playbook-provisioning.groovy
+++ pipeline/playbook-provisioning.groovy
@@ -2,49 +2,50 @@
 *	gitRepo
 *	gitBranch
 *	gitCreds
-*   scriptType
-*   playbook
-*   playbookTags
+*	scriptType
+*	playbook
+*	playbookTags
 */
 
 node ('master') {
 	scriptsDirectory    = "${scriptType}"
-    playbook            = "${playbook}"
-    playbookTags        = "${playbookTags}"
+	playbook            = "${playbook}"
+	playbookTags        = "${playbookTags}"
     
-    date				= new Date()
+	date				= new Date()
 	println date
-    
-    writeFile(file: "askp-${BUILD_TAG}",text:"#!/bin/bash/\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) \"\${STASH_PASWORD}\";;\nesac")
+	
+	writeFile(file: "askp-${BUILD_TAG}",text:"#!/bin/bash/\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) \"\${STASH_PASWORD}\";;\nesac")
 	sh "chmod a+x askp-${BUILD_TAG}"
-
+	
 	stage('Checkout') {
-        checkout()
-    }
-    dir(scriptsDirectory) {
-        stage('Playbook Execution') {
-            ansiColor('xterm') {
-                ansiblePlaybook( 
-                    playbook        : "${playbook}',
-                    tags            : "${playbookTags}"
-                    colorized       : true
-                )
-            }
-        }
-    }
-    
+		checkout()
+	}
+	
+	dir(scriptsDirectory) {
+		stage('Playbook Execution') {
+			ansiColor('xterm') {
+				ansiblePlaybook(
+					playbook        : "${playbook}',
+					tags            : "${playbookTags}"
+					colorized       : true
+				)
+			}
+		}
+	}
+}
 def checkout() {
 	checkout([
-		$class                            : 'GitSCM', 
-		branches                          : [[name      : gitBranch ]], 
-		doGenerateSubmoduleConfigurations : false, 
-		clearWorkspace                    : true,
-		extensions                        : [[$class    : 'CleanCheckout' ],[
-			$class                                      : 'SubmoduleOption', 
+		$class								: 'GitSCM', 
+		branches							: [[name	: gitBranch ]], 
+		doGenerateSubmoduleConfigurations	: false, 
+		clearWorkspace						: true,
+		extensions							: [[$class	: 'CleanCheckout' ],[
+			$class					: 'SubmoduleOption', 
 			disableSubmodules		: false,
 			parentCredentials		: true,
 			recursiveSubmodules		: true,
-			reference               : '',
+			reference				: '',
 			trackingSubmodules		: false]],
 		submoduleCfg				: [],
 		userRemoteConfigs			: [[credentialsId: gitCreds, url: gitRepo]]])
