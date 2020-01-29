a532a5659a3b487c326bad938823c779b8996c30
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-01-29 22:45:22 +0530

    Updated subnet to eni_subnet

--- pipeline/tf-eni-build-1.groovy
+++ pipeline/tf-eni-build-1.groovy
@@ -6,7 +6,7 @@
 *	tfstateBucket
 *	tfstateBucketPrefix
 
-*	subnet
+*	eni_subnet
 *	instance_name
 
 *	includeENI
@@ -97,7 +97,7 @@ def checkout() {
 def set_env_variables() {
 	env.TF_VAR_instance_name	= "${instance_name}"
 	env.TF_VAR_aws_account_num	= "${awsAccount}"
-	env.TF_VAR_subnet           = "${subnet}"
+	env.TF_VAR_eni_subnet           = "${eni_subnet}"
 }
 
 def terraform_init() {
