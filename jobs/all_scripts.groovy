7a4ff4303118eaea6a1fad6608c783868eb66020
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-02-04 07:22:25 +0530

    Added inventory

--- jobs/all_scripts.groovy
+++ jobs/all_scripts.groovy
@@ -11,6 +11,7 @@ pipelineJob('playbook-provisioning-job') {
 		choiceParam('gitCreds'					, [gitCreds]								, '')
 		choiceParam('scriptType'				, ['select','ansible','python','pgsql']		, '')
 		stringParam('playbook'					, 'site.yml'								, '')
+		stringParam('inventory'					, 'inventory'								, '')
 		stringParam('extraVars'					, ''										, '')
 		stringParam('playbookTags'				, ''										, '')
     }
