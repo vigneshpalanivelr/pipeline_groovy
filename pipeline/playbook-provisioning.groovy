9f9f7f02ec940bec8775f90520d420418d255a61
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-02-04 08:25:23 +0530

    Added playbook exection with all examples

--- pipeline/playbook-provisioning.groovy
+++ pipeline/playbook-provisioning.groovy
@@ -7,7 +7,7 @@
 *	playbook
 *	inventory
 *	playbookTags
-*	extraVars
+*	extras
 */
 
 node ('master') {
@@ -29,7 +29,27 @@ node ('master') {
 	
 	dir(scriptsDirectory) {
 		if (extraVars) {
+			stage('Playbook Execution') {
+				ansiColor('xterm') {
+					ansiblePlaybook(
+						playbook        : "${playbook}",
+						inventory		: "${inventory}"
+						tags            : "${playbookTags}",
+						extras			: "${extras}",
+						colorized       : true
+						
+						# Example Options
+						//extras		: "-e group_name='root_group' -e action='create_group'",
+						//extraVars		: [group_name:"root_group", action:"create_group"],
+					)
+				}
+			}
+		}
+		# Example block
+		/*
+		if (extraVars) {
 			stage('Playbook Execution with extravars') {
+				env.ANSIBLE_FORCE_COLOR=true
 				sh "/usr/bin/ansible-playbook ${playbook} --inventory ${inventory} --tags=${playbookTags} --extra-vars '${extraVars}'"
 			}
 		}
@@ -45,6 +65,7 @@ node ('master') {
 				}
 			}
 		}
+		*/
 	}
 }
 def checkout() {
