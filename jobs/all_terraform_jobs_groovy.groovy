ad64f4f8f719befe2ad3e2733119c20199238c80
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-02-22 12:14:45 +0530

    Added SNS topic

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -4,6 +4,7 @@ def gitCreds						= "gitCreds"
 def awsAccount						= "210315133748"
 def tfStateBucket					= "terraform-tfstate-mumba-1"
 
+def tfStateBucketPrefixSNS			= "sns_module"
 def tfStateBucketPrefixS3			= "s3_module"
 def tfStateBucketPrefixS3Log		= "s3_log_module"
 def tfStateBucketPrefixRDS			= "rds_module"
@@ -168,6 +169,31 @@ pipelineJob('tf-kms-key-build-1-job') {
 	definition {
 		cps {
 			script(readFileFromWorkspace('pipeline/tf-kms-key-build-1.groovy'))
+			sandbox()
+		}
+	}
+}
+
+// AWS SNS Topic Creation
+pipelineJob('tf-sns-build-1-job') {
+	description('Building AWS KMS key creation')
+	logRotator(-1,-1)
+	parameters{
+		choiceParam('gitRepo'				, [terraformRepo]				, '')
+		choiceParam('gitBranch'				, [terraformBranch]				, '')
+		choiceParam('gitCreds'				, [gitCreds]					, '')
+		choiceParam('awsAccount'			, [awsAccount]					, '')
+		choiceParam('tfstateBucket'			, [tfStateBucket]				, 'TF State Bucket'             )
+		choiceParam('tfstateBucketPrefix'	, [tfStateBucketPrefixSNS]		, 'TF State Bucket Prefix'      )
+		stringParam('sns_topic_name'		, 'test-sns-topic'				, '')
+		choiceParam('sns_protocol'			, ['email','sms']				, '')
+		choiceParam('sns_endpoint'			, ['vignesh1650@gmail.com']		, '')
+		choiceParam('includeSNS'			, ['true','false']				, '')
+		choiceParam('terraformApplyPlan'	, ['plan','apply','plan-destroy','destroy']	, '')
+	}
+	definition {
+		cps {
+			script(readFileFromWorkspace('pipeline/tf-sns-build-1.groovy'))
 			sandbox()
 		}
 	}
