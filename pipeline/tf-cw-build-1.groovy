3ee8ca60902f78c2f30d151abb25d876a198bc33
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-02-26 01:30:24 +0530

    Updated global vars

--- pipeline/tf-cw-build-1.groovy
+++ pipeline/tf-cw-build-1.groovy
@@ -15,7 +15,7 @@
 node ('master'){
 	terraformDirectoryCW		= "modules/all_modules/${tfstateBucketPrefixEC2CW}/cw_ec2"
 	
-	global_tfvars				= "../../../variables/global_vars.tfvars"
+	global_tfvars				= "../../../../variables/global_vars.tfvars"
 	cw_tfvars					= "../../../../variables/cw_vars.tfvars"
 	
 	date						= new Date()
