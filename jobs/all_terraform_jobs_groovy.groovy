7e7d9de8d3c505ee0cd84dadbe2c6f90f6a4b8be
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-02-29 12:20:55 +0530

    Updated SG name in ENI

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -239,7 +239,7 @@ pipelineJob('tf-eni-build-1-job') {
 		choiceParam('tfstateBucket'			, [tfStateBucket]				, 'TF State Bucket'             )
 		choiceParam('tfstateBucketPrefix'	, [tfStateBucketPrefixENI]		, 'TF State Bucket Prefix'      )
 		choiceParam('eni_subnet'			, ['default-subnet-1','default-subnet-2','default-subnet-3']	, 'ENI Subnet'	)
-		stringParam('sg_group_name'			, 'default-ec2-sg'				, 'ENI Security Group'			)
+		stringParam('sg_group_name'			, 'test-instance'				, 'ENI Security Group'			)
 		stringParam('resource_name'			, 'test-instance'				, '')
 		choiceParam('includeENI'			, ['true','false']				, '')
 		choiceParam('terraformApplyPlan'	, ['plan','apply','plan-destroy','destroy']	, '')
