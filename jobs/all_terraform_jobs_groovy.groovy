86338ea4c245c801d4987bac906782d2451736d4
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2020-05-24 15:58:39 +0530

    Added Lambda Function

--- jobs/all_terraform_jobs_groovy.groovy
+++ jobs/all_terraform_jobs_groovy.groovy
@@ -20,6 +20,7 @@ def tfStateBucketPrefixEBS			= "ebs_module"
 def tfStateBucketPrefixEBSAttach	= "ebs_attachment_module"
 def tfStateBucketPrefixEC2			= "ec2_module"
 def tfStateBucketPrefixEC2CW		= "cw_module"
+def tfStateBucketPrefixLambda		= "lambda_module"
 
 // RDS DB Build Generic Job
 pipelineJob('tf-rds-db-build-1-job') {
@@ -347,6 +348,31 @@ pipelineJob('tf-ec2-build-1-job') {
 	definition {
 		cps {
 			script(readFileFromWorkspace('pipeline/tf-ec2-build-1.groovy'))
+			sandbox()
+		}
+	}
+}
+
+// AWS Lambda Creation
+pipelineJob('tf-lambda-build-1-job') {
+	description('Building AWS ENI creation')
+	logRotator(100,100)
+	parameters{
+		choiceParam('gitRepo'				, [terraformRepo]				, '')
+		choiceParam('gitBranch'				, [terraformBranch]				, '')
+		choiceParam('gitCreds'				, [gitCreds]					, '')
+		choiceParam('awsAccount'			, [awsAccount]					, '')
+		choiceParam('tfstateBucket'			, [tfStateBucket]				, 'TF State Bucket'             )
+		choiceParam('tfstateBucketPrefix'	, [tfStateBucketPrefixLambda]	, 'TF State Bucket Prefix'      )
+		choiceParam('lambda_subnet'			, ['default-subnet-1','default-subnet-2','default-subnet-3']	, 'ENI Subnet'	)
+		stringParam('sg_group_name'			, 'test-instance-sg'			, 'ENI Security Group'			)
+		choiceParam('lambda_function'		, ['ec2_stop_scheduler']		, '')
+		choiceParam('includeENI'			, ['true','false']				, '')
+		choiceParam('terraformApplyPlan'	, ['plan','apply','plan-destroy','destroy']	, '')
+	}
+	definition {
+		cps {
+			script(readFileFromWorkspace('pipeline/tf-lambda-build-1.groovy'))
 			sandbox()
 		}
 	}
---
+++ pipeline/tf-lambda-build-1.groovy
@@ -0,0 +1,134 @@
+/*
+*	gitRepo
+*	gitBranch
+*	gitCreds
+*	awsAccount
+*	tfstateBucket
+*	tfstateBucketPrefix
+
+*	lambda_subnet
+*	sg_group_name
+*	lambda_function
+
+*	includeLambda
+*	terraformApplyPlan
+*/
+
+node ('master'){
+	terraformDirectory	= "modules/all_modules/${tfstateBucketPrefix}"
+	
+	global_tfvars		= "../../../variables/global_vars.tfvars"
+	lambda_tfvars		= "../../../variables/lambda_vars.tfvars"
+	
+	date				= new Date()
+	println date
+
+	writeFile(file: "askp-${BUILD_TAG}",text:"#!/bin/bash/\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) \"\${STASH_PASWORD}\";;\nesac")
+	sh "chmod a+x askp-${BUILD_TAG}"
+
+	stage('Checkout') {
+		checkout()
+		if (includeLambda == 'true') {
+			dir(terraformDirectory) {
+				stage('Remote State Init') {
+					terraform_init(tfstateBucketPrefix, lambda_function, 'lambda')
+				}
+				if (terraformApplyPlan == 'plan' || terraformApplyPlan == 'apply') {
+					stage('Terraform Plan') {
+						set_env_variables()
+						terraform_plan(global_tfvars,lambda_tfvars)
+					}
+				}
+				if (terraformApplyPlan == 'apply') {
+					stage('Approve Plan') {
+						approval()
+					}
+					stage('Terraform Apply') {
+						terraform_apply()
+					}
+				}
+				if (terraformApplyPlan == 'plan-destroy' || terraformApplyPlan == 'destroy') {
+					stage('Plan Destroy') {
+						set_env_variables()
+						terraform_plan_destroy(global_tfvars,lambda_tfvars)
+					}
+				}
+				if (terraformApplyPlan == 'destroy') {
+					stage('Approve Destroy') {
+						approval()
+					}
+					stage('Destroy') {
+						terraform_destroy()
+					}
+				}
+			}
+		}
+	}
+}
+
+def approval() {
+	timeout(time: 5, unit: 'DAYS') {
+		input(
+			id: 'Approval', message: 'Shall i continue ?', parameters: [[
+				$class:	'BooleanParameterDefinition', defaultValue: true, description: 'default to tick', name: 'Please confirm to proceed']]
+		)
+	}
+}
+
+def checkout() {
+	checkout([
+		$class                            : 'GitSCM', 
+		branches                          : [[name		: gitBranch ]], 
+		doGenerateSubmoduleConfigurations : false, 
+		clearWorkspace                    : true,
+		extensions                        : [[$class	: 'CleanCheckout' ],[
+			$class					: 'SubmoduleOption', 
+			disableSubmodules		: false,
+			parentCredentials		: true,
+			recursiveSubmodules		: true,
+			reference				: '',
+			trackingSubmodules		: false]],
+		submoduleCfg				: [],
+		userRemoteConfigs			: [[credentialsId: gitCreds, url: gitRepo]]])
+}
+
+def set_env_variables() {
+	env.TF_VAR_lambda_function	= "${lambda_function}"
+	env.TF_VAR_aws_account_num	= "${awsAccount}"
+	env.TF_VAR_vpc_subnet_name	= "${lambda_subnet}"
+	env.TF_VAR_sg_group_name	= "${sg_group_name}"
+}
+
+def terraform_init(module, tfstatename, stack) {
+	withEnv(["GIT_ASKPASS=${WORKSPACE}/askp-${BUILD_TAG}"]){
+		withCredentials([usernamePassword(credentialsId: gitCreds, usernameVariable: 'STASH_USERNAME', passwordVariable: 'STASH_PASSWORD')]) {
+			wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+				sh "terraform init -input=false -upgrade=true -backend=true -force-copy -backend-config='bucket=${tfstateBucket}' -backend-config='key=${module}/${tfstatename}-${stack}.tfstate'"
+			}
+		}
+	}
+}
+
+def terraform_plan(global_tfvars,lambda_tfvars) {
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+		sh "terraform plan -out=tfplan -input=false -var-file=${global_tfvars} -var-file=${lambda_tfvars}"
+	}
+}
+
+def terraform_apply() {
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+		sh "terraform apply -input=false tfplan"
+	}
+}
+
+def terraform_plan_destroy(global_tfvars,lambda_tfvars) {
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+		sh "terraform plan -destroy -out=tfdestroy -input=false -var-file=${global_tfvars} -var-file=${lambda_tfvars}"
+	}
+}
+
+def terraform_destroy() {
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+		sh "terraform apply -input=false tfdestroy"
+	}
+}
