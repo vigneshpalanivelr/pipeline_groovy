dcb859532b983c80121541dc8c84a72240389c51
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2020-05-27 20:07:39 +0530

    Updated All: Pipeline Jobs Name

--- admin-job/admin_job.groovy
+++ admin-job/admin_job.groovy
@@ -10,19 +10,19 @@
 
 def pipelineGroovyStack		=	"https://github.com/vigneshpalanivelr/pipeline_groovy.git"
 def pipelineGroovyBranch	=	"master"
-def GitCreds			= 	"gitCreds"
-def seedJobDSL			=	"admin-pipeline/admin_seed_job.groovy"
-def jobDSL			=	"jobs/**/*.groovy"
+def GitCreds				= 	"gitCreds"
+def seedJobDSL				=	"admin-pipeline/admin_seed_job.groovy"
+def jobDSL					=	"jobs/**/*.groovy"
 
 //Admin Seed Job
 pipelineJob("admin-seed-job") {
 	description('Job To Create All Admin Seed Jobs')
 	parameters {
-		choiceParam("jobDSL_repo"	, [pipelineGroovyStack]	, "Job DSL Repo")
-		choiceParam("jobDSL_branch"	, [pipelineGroovyBranch], "Job DSL Branch")
-		choiceParam("jobDSL_creds"	, [GitCreds]		, "Job DSL Cred")
-		choiceParam("jobDSL_path"	, [seedJobDSL]		, "Location of Job DSL Groovy Script")
-		choiceParam("jobDSL_scripts"	, [jobDSL]		, "Location of Job DSL Groovy Script")
+		choiceParam("jobDSL_repo"		, [pipelineGroovyStack]		, "Job DSL Repo")
+		stringParam("jobDSL_branch"		, pipelineGroovyBranch		, "Job DSL Branch")
+		choiceParam("jobDSL_creds"		, [GitCreds]				, "Job DSL Cred")
+		choiceParam("jobDSL_path"		, [seedJobDSL]				, "Location of Job DSL Groovy Script")
+		choiceParam("jobDSL_scripts"	, [jobDSL]					, "Location of Job DSL Groovy Script")
 	}
 	definition {
 		cps {
